ac38e8dc48b5b88fa65239c50a921b395b9dc05f
Raj Kumar <raj.kumar@bewotechno.com>
2016-11-15 13:46:43 +0530

    Initial Update 2

---
+++ app/src/main/java/bewo/mqttsample/MainActivity.java
@@ -0,0 +1,53 @@
+package bewo.mqttsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MainActivity extends AppCompatActivity {
+
+    String topic        = "camp_logs";
+    String content      = "Message from Raj";
+    int qos             = 0;
+    String broker       = "tcp://103.228.83.72:1883";
+    String clientId     = "Raj Id";
+    MemoryPersistence persistence = new MemoryPersistence();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setUserName("test");
+            char[] chars = {'t','e','s','t'};
+            connOpts.setPassword(chars);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            //System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+}
